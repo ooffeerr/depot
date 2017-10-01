f6dccb2193ce1b464f19c7abc7f44c51990944e6
Ofer Schonberger <oschonberger@il-017252-mbp.local>
2017-10-01 11:55:46 +0300

    of testing

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,7 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select '#columns #side a', minimum: 4
   end
 
 end
